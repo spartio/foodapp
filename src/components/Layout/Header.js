5c4c32e717c1f37052e458e275d74f09683685a1
Simo Partio <simo.partio@outlook.com>
2021-10-30 23:35:31 +0300

    first commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import styles from "./Header.module.css";
+import mealsImage from '../../assets/meals.jpg'
+import { Fragment } from "react";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={styles.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart}>Cart</HeaderCartButton>
+      </header>
+      <div class={styles['main-image']}>
+        <img src={mealsImage} alt="A table of full of food"/>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,43 @@
+import styles from "./HeaderCartButton.module.css";
+import CartIcon from "../Cart/CartIcon";
+import { useContext, useEffect, useState } from "react";
+import CartContext from "../../store/cart-context";
+
+const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  const btnStyles = `${styles.button} ${btnIsHighlighted ? styles.bump : ""}`;
+  const { items } = cartCtx;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+    
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  return (
+    <button className={btnStyles} onClick={props.onClick}>
+      <span className={styles.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={styles.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
